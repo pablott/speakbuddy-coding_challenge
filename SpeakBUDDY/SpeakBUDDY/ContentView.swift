20efece0720d9c0beb6254817528ea3a4e712b0e
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2023-12-03 00:23:11 +0900

    Initial project commit

---
+++ SpeakBUDDY/SpeakBUDDY/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  SpeakBUDDY
+//
+//  Created by Pablo Trabajos on 02/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeakBUDDY/SpeakBUDDY/SpeakBUDDYApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakBUDDYApp.swift
+//  SpeakBUDDY
+//
+//  Created by Pablo Trabajos on 02/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeakBUDDYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
