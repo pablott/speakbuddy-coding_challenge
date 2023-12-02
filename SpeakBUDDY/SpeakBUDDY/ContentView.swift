3f8c0a6bc15684871e1a30cc83d44d68b8c566f8
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2023-12-03 00:26:28 +0900

    Add basic layout

--- SpeakBUDDY/SpeakBUDDY/ContentView.swift
+++ SpeakBUDDY/SpeakBUDDY/ContentView.swift
@@ -9,6 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack {
+            Button {
+                print("Close was tapped")
+            } label: {
+                Image(systemName: "xmark")
+                    .fontWeight(.bold)
+                    .padding()
+                    .foregroundColor(Color("textColor"))
+                    .background(.gray)
+                    .clipShape(Circle())
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            }
+
+
+            Text("Hello\nSpeakBUDDY")
+                .font(.largeTitle)
+            Image("protty")
+            Rectangle()
+                .fill(Gradients().barGradient)
+            Text("スピークバディで")
+            Text("レベルアップ")
+                .background(Gradients().barGradient)
+
+            Button("プランに登録する") {
+                print("Sign up was tapped")
+            }
+            .background(Color("buttonColor"))
+        }
+        .padding()
+        .background(Gradients().bgGradient)
+
     }
 }
 
